66a717c53f7079b3fece0790824569b2b3ba4b11
Destru1 <68596367+Destru1@users.noreply.github.com>
2022-03-31 17:55:52 +0300

    Add tasks

---
+++ LinkedListTask/LinkedListTask/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < 30; i++)
+            {
+                int value = random.Next(1, 99);
+                list.AddLast(value);
+            }
+
+            foreach (int number in list)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter value of x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                var node = list.First;
+
+                while (node != null)
+                {
+                    var nextNode = node.Next;
+                    if (node.Value > x && node.Value < y)
+                    {
+                        list.Remove(node);
+                    }
+                    node = nextNode;
+                }
+
+            }
+
+            Console.WriteLine($"List without numbers between {x} and {y} :");
+            foreach (var number in list)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("==============================================");
+
+            int[] array = new int[list.Count];
+            list.CopyTo(array, 0);
+
+            Console.WriteLine($"Array with numbers devidable by {x} or {y} ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = array[i];
+                bool isDevidable = number % x == 0 || number % y == 0;
+                if (isDevidable)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+        }
+    }
+}
+    
+
---
+++ StackToQueue/StackToQueue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackToQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+
+            int value = int.Parse(Console.ReadLine());
+         
+
+            while (value > 0)
+            {
+               
+                stack.Push(value);
+                value = int.Parse(Console.ReadLine());
+                
+            }
+
+            Console.Write("Elements in stack: ");
+            foreach (var numb in stack)
+            {
+                Console.Write(numb + " ");
+            }
+           
+
+            int[] arr = stack.ToArray();
+         
+
+            Queue queue = new Queue();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int number = arr[i];
+                if (number %2 == 0)
+                {
+
+                    queue.Enqueue(number);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Queue with even numbers:");
+            foreach (var element in queue)
+            {
+                Console.Write(element + " ");
+            }
+
+        }
+    }
+}
