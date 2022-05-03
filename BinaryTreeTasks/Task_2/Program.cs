abaf1c034d5ed9fa5f697d8c79306226163ee066
Destru1 <68596367+Destru1@users.noreply.github.com>
2022-05-03 15:55:20 +0300

    Add new tasks

---
+++ BinaryTreeTasks/Task_2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        private const int N = 30;
+
+        private static Node node = new Node();
+        private static Tree tree = new Tree();
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            Random random = new Random();
+
+            int[] array = new int[N];
+            Console.WriteLine("Initial array");
+            for (int i = 0; i < N; i++)
+            {
+                int number = random.Next(1, 100);
+                array[i] = number;
+                Console.Write(array[i] + " ");
+            }
+
+            node.value = array[0];
+            int value = 0;
+            for (int i = 1; i < N; i++)
+            {
+                value = array[i];
+                tree.Add(ref node, value);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted array with LNR traverse");
+
+            int index = 0;
+            void Sort(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+                Sort(root.left);
+                array[index++] = root.value;
+                Sort(root.right);
+            }
+            Sort(node);
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                Console.Write(array[i] + " ");
+            }
+
+        }
+
+
+        public class Node
+        {
+            public int value;
+            public Node left;
+            public Node right;
+        }
+
+        public class Tree
+        {
+
+            public void Add(ref Node root, int number)
+            {
+                if (root == null)
+                {
+                    root = new Node();
+                    root.value = number;
+
+                    return;
+                }
+
+
+                if (number < root.value)
+                {
+                    Add(ref root.left, number);
+                }
+                else
+                {
+                    Add(ref root.right, number);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ BinaryTreeTasks/Task_3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static Node node = new Node();
+        public static Tree tree = new Tree();
+        public static LinkedList<int> primeNumbers = new LinkedList<int>();
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+
+            FindPrimeNumbers(primeNumbers);
+            Console.WriteLine("Prime numbers:");
+            Console.WriteLine(String.Join(" ", primeNumbers));
+            Console.WriteLine("Tree");
+            FillTree();
+            tree.Traverse(node);
+
+        }
+
+        private static void FindPrimeNumbers(LinkedList<int> primeNumbers)
+        {
+            for (int i = 2; i < 1000; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                    if (count == 2) break;
+
+                }
+                if (count != 2)
+                    if (i > 100)
+                        primeNumbers.AddLast(i);
+
+            }
+        }
+
+        public static void FillTree()
+        {
+            var firstNode = primeNumbers.First;
+            int number;
+            
+
+            while (firstNode != null)
+            {
+
+
+                var nextNode = firstNode.Next;
+                if (firstNode.Value % 10 == 7)
+                {
+
+                    number = firstNode.Value;
+                    tree.Add(ref node, number);
+                }
+                firstNode = nextNode;
+            }
+
+        }
+        public class Node
+        {
+            public int value;
+            public Node left;
+            public Node right;
+        }
+
+        public class Tree
+        {
+
+            public void Add(ref Node root, int number)
+            {
+                if (root == null)
+                {
+                    root = new Node();
+                    root.value = number;
+
+                    return;
+                }
+
+
+                if (number < root.value)
+                {
+                    Add(ref root.left, number);
+                }
+                else
+                {
+                    Add(ref root.right, number);
+                }
+            }
+
+
+            public void Traverse(Node node)
+            {
+                Console.Write(node.value + " ");
+
+                if (node.left != null)
+                {
+                    Traverse(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    Traverse(node.right);
+                }
+
+            }
+        }
+    }
+}
